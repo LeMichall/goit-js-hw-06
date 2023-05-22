36ae7f114f9b658068ec2eb5157cfba98e28d4f7
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-23 01:28:37 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+const liArray = [];
+ingredients.forEach((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.classList.add("item");
+  liArray.push(li);
+});
+list.append(...liArray);
