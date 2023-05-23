9c81d7ac3462040d3aa64e6cb762621e1964bc54
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-23 21:18:21 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", submitHandler);
 
+function submitHandler(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+  const credentials = {
+    email: email.value,
+    password: password.value,
+  };
+  if (email.value === "" || password.value === "") {
+    alert("All fields should be filled!");
+    return;
+  }
+  console.log(credentials);
+  evt.currentTarget.reset();
+}
