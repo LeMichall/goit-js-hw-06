74eb4c8f86520b0c1daa088dd77ffbab2993fc93
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-23 20:28:48 +0200

    task 7

---
+++ js/task-07.js
@@ -0,0 +1,5 @@
+const fontSizeControl = document.querySelector("#font-size-control");
+const spanText = document.querySelector("#text");
+fontSizeControl.addEventListener("input", (event) => {
+  spanText.style.fontSize = `${fontSizeControl.value}px`;
+});
