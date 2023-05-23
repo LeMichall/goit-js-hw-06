9df5d42b57af3503f6b2ff43e30bc2e2cae386ef
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-23 21:31:45 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorName = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+console.log(btn);
+btn.addEventListener("click", colorChange);
+
+function colorChange(color) {
+  color = getRandomHexColor();
+  console.log(color);
+  document.body.style.backgroundColor = color;
+  colorName.textContent = color;
+}
